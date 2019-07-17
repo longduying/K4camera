669156f5a8a35150c25e0290f7372497486ca2fb
longduying <50438575+longduying@users.noreply.github.com>
2019-07-17 09:33:50 +0800

    修改
    修改

---
+++ camera/src/components/OrderManage.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="orderManage">
+        <div class="Dhead">订单列表</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "OrderManage"
+    }
+</script>
+
+<style scoped>
+ .Dhead{
+     height: 50px;
+     background: white;
+     line-height: 50px;
+     border-bottom: 1px solid gainsboro;
+ }
+</style>
