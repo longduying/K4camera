130df0f99c1b30777cbeede1417e519b0be5c6ba
gongchangyueban <50438587+gongchangyueban@users.noreply.github.com>
2019-07-17 11:01:47 +0800

    上传图标

---
+++ camera/src/views/Login.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <div>
+            <div>
+                <p>欢迎登陆雷霆数码</p>
+                <p>后台管理系统</p>
+            </div>
+            <div>
+                账号：<input type="text">
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Login"
+    }
+</script>
+
+<style scoped>
+
+</style>
